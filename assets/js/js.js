90c89f47cd3c7e067902a453cfce1ebe8ccaeb0a
KiranKumarMalik <malikkiran413@gmail.com>
2023-03-29 19:34:23 +0530

    changes in design code

--- assets/js/js.js
+++ assets/js/js.js
@@ -302,6 +302,42 @@ sr.reveal(`.sr06`, { delay: 300, origin: 'top' }),
       }
       cssSlidy();
 
+
+      document.onkeydown = function(e)
+    {
+        if(event.keyCode == 123)
+        {
+            return false;
+        }
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0))
+        {
+            return false;
+        }
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0))
+        {
+            return false;
+        }
+        if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))
+        {
+            return false;
+        }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0))
+    {
+      return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
+        return false;
+     }
+     if(e.ctrlKey && e.keyCode == 'P'.charCodeAt(0)) {
+        return false;
+     }
+     if(e.ctrlKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+     }
+     if(e.ctrlKey && e.keyCode == 'V'.charCodeAt(0)) {
+        return false;
+     }
+    }
       
 
     
